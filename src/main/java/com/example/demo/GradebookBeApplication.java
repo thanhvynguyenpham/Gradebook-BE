ba0d4179fe0d2c23597536f7e7817fd279db0e5b
thanhvynguyenpham <thanhvy.forwork@gmail.com>
2021-10-31 09:03:38 +0700

    initial commit

---
+++ src/main/java/com/example/demo/GradebookBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradebookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradebookBeApplication.class, args);
+	}
+
+}
